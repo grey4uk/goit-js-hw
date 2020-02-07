d2969030e3cb7cf51bb603757e8ca617f43e4bc4
grey4uk <grey4uk14@gmail.com>
2020-02-07 12:10:12 +0200

    correct reaction on enter

--- hw08/js/gallery-items.js
+++ hw08/js/gallery-items.js
@@ -1,8 +1,9 @@
+'use strict'
 import images from "./images.js";
 const imagesList = document.querySelector(".gallery");
 imagesList.addEventListener("click", onImgClick);
 // debugger;
-console.log(imagesList);
+// console.log(imagesList);
 function createImagesList(imagesObj) {
   return imagesObj.reduce((acc, image) => {
     return (acc = acc.concat(
@@ -19,23 +20,24 @@ const closeModalOnBtnClick = document.querySelector(".lightbox__button");
 
 // debugger;
 function onImgClick(e) {
-  console.log(e);
+  // console.log('-->',e.target !== e.currentTarget);
+  // e.preventDefault();
   const clickImg = e.target;
-  if (e.target === e.currentTarget) {
-    return;
-  }
-    lightBoxFocus.classList.add("is-open");
-    lightBoxImgFocus.src = clickImg.dataset.source;
-    closeModal.addEventListener("click", closeModalOnClick);
-    window.addEventListener("keydown", closeModalOnEsc);
-    closeModalOnBtnClick.addEventListener("click", closeModalOnClick);
+  if (clickImg.tagName === "A"){return;}
+  if (e.target !== e.currentTarget) {
+  lightBoxFocus.classList.add("is-open");
+  lightBoxImgFocus.src = clickImg.dataset.source;
+  window.addEventListener("keydown", closeModalOnEsc);
+  closeModal.addEventListener("click", closeModalOnClick);
+  closeModalOnBtnClick.addEventListener("click", closeModalOnClick);}
+  return;
 }
 
 function closeModalWindow() {
   lightBoxFocus.classList.remove("is-open");
   lightBoxImgFocus.src = "";
-  closeModal.removeEventListener("click", closeModalOnClick);
   window.removeEventListener("keydown", closeModalOnEsc);
+  closeModal.removeEventListener("click", closeModalOnClick);
   closeModalOnBtnClick.removeEventListener("click", closeModalOnClick);
 }
 
@@ -47,6 +49,8 @@ function closeModalOnClick(e) {
 }
 
 function closeModalOnEsc(e) {
+  //  console.log(e);
+  e.preventDefault();
   if (e.code !== "Escape") {
     return;
   }
