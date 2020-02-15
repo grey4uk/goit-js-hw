7919460ff75b9e5f4ef8e3f0a5369e7449159e74
grey4uk <grey4uk14@gmail.com>
2020-02-15 21:08:53 +0200

    modification for timer

--- hw11/11_3/js/script.js
+++ hw11/11_3/js/script.js
@@ -1,46 +1,13 @@
-// class CountdownTimer {
-//   constructor(item) {
-//     this.selector = item.selector;
-//     this.targetDate = item.targetDate;
-//   }
-//   days(time) {
-//     return Math.floor(time / (1000 * 60 * 60 * 24));
-//   }
-//   hours(time) {
-//     return Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   }
-//   mins(time) {
-//     return Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-//   }
-//   secs(time) {
-//     return Math.floor((time % (1000 * 60)) / 1000);
-//   }
-// }
-
-// const countdownTimer = new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Feb 18, 2020")
-// });
-// function time(){
-//   return countdownTimer.targetDate.getTime() - Date.now();
-// };
-
-// function setCountdown(targetCountdown,valueCountdown) {
-//   setInterval(() => {
-//     document
-//       .querySelector(countdownTimer.selector)
-//       .querySelector(targetCountdown).textContent = valueCountdown(time());
-//   }, 1000);
-// }
-// setCountdown('span[data-value="secs"]',countdownTimer.secs);
-// setCountdown('span[data-value="mins"]',countdownTimer.mins);
-// setCountdown('span[data-value="hours"]',countdownTimer.hours);
-// setCountdown('span[data-value="days"]',countdownTimer.days);
 class CountdownTimer {
   constructor(item) {
     this.selector = item.selector;
     this.targetDate = item.targetDate;
-    this.setCountdown('span[data-value="secs"]','span[data-value="mins"]','span[data-value="hours"]','span[data-value="days"]');
+    this.setCountdown(
+      'span[data-value="secs"]',
+      'span[data-value="mins"]',
+      'span[data-value="hours"]',
+      'span[data-value="days"]'
+    );
   }
   days(time) {
     return Math.floor(time / (1000 * 60 * 60 * 24));
@@ -54,37 +21,29 @@ class CountdownTimer {
   secs(time) {
     return Math.floor((time % (1000 * 60)) / 1000);
   }
-  time(){
-    return this.targetDate.getTime() - Date.now();
-  };
-  setCountdown(seconds,minutes,hours,days) {
+  time() {
+    let time = this.targetDate.getTime() - Date.now();
+    if (time >= 0) {
+      return time;
+    } else {
+      // alert("Date is here)))");
+      document.querySelector(this.selector).style.display = "none";
+      document.querySelector("h1").style.display = "none";
+      document.querySelector("body").style.backgroundImage =
+        'url("https://picua.org/images/2020/02/14/3a1ddd2212690c1e282a028bb48b3879.jpg")';
+    }
+  }
+  setCountdown(seconds, minutes, hours, days) {
     setInterval(() => {
-        document.querySelector(seconds).textContent = this.secs(this.time());
-        document.querySelector(minutes).textContent = this.mins(this.time());
-        document.querySelector(hours).textContent = this.hours(this.time());
-        document.querySelector(days).textContent = this.days(this.time());
+      document.querySelector(seconds).textContent = this.secs(this.time());
+      document.querySelector(minutes).textContent = this.mins(this.time());
+      document.querySelector(hours).textContent = this.hours(this.time());
+      document.querySelector(days).textContent = this.days(this.time());
     }, 1000);
   }
 }
-
+const myDate=prompt("input data, such in example: Feb 15, 2020, 20:51");
 const countdownTimer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Feb 15, 2020")
+  targetDate: new Date(`${myDate}`)
 });
-
-
-// function time(){
-//   return countdownTimer.targetDate.getTime() - Date.now();
-// };
-
-// function setCountdown(targetCountdown,valueCountdown) {
-//   setInterval(() => {
-//     document
-//       .querySelector(countdownTimer.selector)
-//       .querySelector(targetCountdown).textContent = valueCountdown(time());
-//   }, 1000);
-// }
-// setCountdown('span[data-value="secs"]',countdownTimer.secs);
-// setCountdown('span[data-value="mins"]',countdownTimer.mins);
-// setCountdown('span[data-value="hours"]',countdownTimer.hours);
-// setCountdown('span[data-value="days"]',countdownTimer.days);
